3b167a772d9097a7e93f794f714f8db1b13503c0
ianhflores <ihflores@uci.edu>
2020-11-19 11:43:07 -0800

    EAR-3 Added class structure

--- network.py
+++ network.py
@@ -19,102 +19,145 @@ from tensorflow.keras.preprocessing import image
 
 from dataset import EARDataset
 
+class Network:
+    #default network parameters
+    EPOCHS = 20
+    KERNEL_SIZE = 5
 
-# directories
-LOG_DIR = './logs/fit/'
+    #default directories
+    LOG_DIR = './LOGS/FIT/'
+    
+    def __init__(self, training_data_path, validation_data_path) -> None:
+        #paths to data samples
+        self.training_data_path = training_data_path
+        self.validation_data_path = validation_data_path
+        
+        #training dataset
+        self.dataset = None
+        
+        #Dataset variables
+        self.BATCH_SIZE = None
+        self.NUM_CLASSES = None
+        
+        #training data
+        self.train_x = None
+        self.train_y = None
+        
+        #validation data
+        self.test_x = None
+        self.test_y = None
+        
+        #one_hot labels
+        self.train_y_one_hot = None
+        self.test_y_one_hot = None
+        
+        #network model
+        self.model = None
 
-# path to data samples
-training_data_path = '/home/hiraj/projects/ear-uci-dataset/spectrograms/train'
-validation_data_path = '/home/hiraj/projects/ear-uci-dataset/spectrograms/validation'
+    def prepare_dataset(self) -> None:
+        """Loads the EAR dataset, normalizes, reshapes, and converts labels to one-hot encoding
 
-# load dataset
-dataset = EARDataset()
-dataset.load(training_data_path, validation_data_path)
-train_x, train_y, test_x, test_y = dataset.train_x, dataset.train_y, dataset.test_x, dataset.test_y
+        """
+        # load dataset
+        self.dataset = EARDataset()
+        self.dataset.load(self.training_data_path, self.validation_data_path)
+        self.train_x, self.train_y, self.test_x, self.test_y = self.dataset.train_x, self.dataset.train_y, self.dataset.test_x, self.dataset.test_y
 
-# normalize each spectrogram's values individually from 0.0 to 1.0
-train_x, test_x = map(lambda x: (x[:, :] - x[:, :].min()) / (x[:, :].max() - x[:, :].min()), [train_x, test_x])
+        # normalize each spectrogram's values individually from 0.0 to 1.0
+        self.train_x, self.test_x = map(lambda x: (x[:, :] - x[:, :].min()) / (x[:, :].max() - x[:, :].min()), [self.train_x, self.test_x])
 
-# print data shape
-print('Training data shape : ', train_x.shape, train_y.shape)
-print('Testing data shape : ', test_x.shape, test_y.shape)
+        # print data shape
+        print('Training data shape : ', self.train_x.shape, self.train_y.shape)
+        print('Testing data shape : ', self.test_x.shape, self.test_y.shape)
 
-# print number of classifications
-print('Total number of outputs : ', len(dataset.SAMPLE_CATEGORIES))
-print('Output classes : ', dataset.SAMPLE_CATEGORIES)
+        # print number of classifications
+        print('Total number of outputs : ', len(self.dataset.SAMPLE_CATEGORIES))
+        print('Output classes : ', self.dataset.SAMPLE_CATEGORIES)
 
-# test that data was properly loaded by displaying a couple samples
-plt.figure(figsize=[8, 4])
+        # test that data was properly loaded by displaying a couple samples
+        plt.figure(figsize=[8, 4])
 
-# first spectrogram in training set
-plt.subplot(121)
-plt.imshow(train_x[0, 0, :, :], cmap='gray')
-plt.title("Training Set Ground Truth : {}".format(train_y[0, 0]))
-# first spectrogram in testing set
-plt.subplot(122)
-plt.imshow(test_x[0, 0, :, :], cmap='gray')
-plt.title("Test Set Ground Truth : {}".format(test_y[0, 0]))
+        # first spectrogram in training set
+        plt.subplot(121)
+        plt.imshow(self.train_x[0, 0, :, :], cmap='gray')
+        plt.title("Training Set Ground Truth : {}".format(self.train_y[0, 0]))
+        # first spectrogram in testing set
+        plt.subplot(122)
+        plt.imshow(self.test_x[0, 0, :, :], cmap='gray')
+        plt.title("Test Set Ground Truth : {}".format(self.test_y[0, 0]))
 
-plt.show()
+        plt.show()
 
-# reshape data to flatten all demensions before the spectrograms
-train_x = train_x.reshape(-1, dataset.SAMPLE_SHAPE[0], dataset.SAMPLE_SHAPE[1], 1)
-test_x = test_x.reshape(-1, dataset.SAMPLE_SHAPE[0], dataset.SAMPLE_SHAPE[1], 1)
-train_y = train_y.reshape(-1)
-test_y = test_y.reshape(-1)
-# print reshaped
-print('Training Set Reshape : {} {}'.format(train_x.shape, train_y.shape))
-print('Test Set Reshape : {} {}'.format(test_x.shape, test_y.shape))
+        # reshape data to flatten all demensions before the spectrograms
+        self.train_x = self.train_x.reshape(-1, self.dataset.SAMPLE_SHAPE[0], self.dataset.SAMPLE_SHAPE[1], 1)
+        self.test_x = self.test_x.reshape(-1, self.dataset.SAMPLE_SHAPE[0], self.dataset.SAMPLE_SHAPE[1], 1)
+        self.train_y = self.train_y.reshape(-1)
+        self.test_y = self.test_y.reshape(-1)
+        # print reshaped
+        print('Training Set Reshape : {} {}'.format(self.train_x.shape, self.train_y.shape))
+        print('Test Set Reshape : {} {}'.format(self.test_x.shape, self.test_y.shape))
 
-# format data type
-train_x, test_x = map(lambda x: x.astype('float32'), [train_x, test_x])
+        # format data type
+        self.train_x, self.test_x = map(lambda x: x.astype('float32'), [self.train_x, self.test_x])
 
-# convert labels to one-hot encoding
-train_y_one_hot = utils.to_categorical(train_y)
-test_y_one_hot = utils.to_categorical(test_y)
+        #convert labels to one-hot encoding
+        self.train_y_one_hot = utils.to_categorical(self.train_y)
+        self.test_y_one_hot = utils.to_categorical(self.test_y)
 
-# show the converted labels vs original
-print('Original label: ', train_y[0])
-print('Converted label: ', train_y_one_hot[0])
+        # show the converted labels vs original
+        print('Original label: ', self.train_y[0])
+        print('Converted label: ', self.train_y_one_hot[0])
 
-# Neural Network Structure
-BATCH_SIZE = dataset.SAMPLES_PER_FOLD
-EPOCHS = 20
-NUM_CLASSES = len(dataset.SAMPLE_CATEGORIES)
-KERNEL_SIZE = 5
+    def create_network(self) -> None:
+        """Create the sequential network model using Keras API
+        
+        """
+        # Neural Network Structure
+        self.BATCH_SIZE = self.dataset.SAMPLES_PER_FOLD
+        self.NUM_CLASSES = len(self.dataset.SAMPLE_CATEGORIES)
 
-# build the sequential network
-##NEED TO LOOK INTO DROPOUT (HELPS PREVENT OVERFITTING MODEL)##
-model = keras.Sequential()
-model.add(layers.Conv2D(24, kernel_size=(KERNEL_SIZE, KERNEL_SIZE), activation='relu', padding='same', input_shape=(dataset.SAMPLE_SHAPE[0], dataset.SAMPLE_SHAPE[1], 1)))
-model.add(layers.MaxPooling2D(pool_size=(4, 2), strides=(4, 2), padding ='same'))
-model.add(layers.Dropout(0.25))
-model.add(layers.Conv2D(48, (KERNEL_SIZE, KERNEL_SIZE), activation='relu', padding='same'))
-model.add(layers.MaxPooling2D(pool_size=(4, 2), strides=(4, 2), padding ='same'))
-model.add(layers.Dropout(0.25))
-model.add(layers.Conv2D(48, (KERNEL_SIZE, KERNEL_SIZE), activation='relu', padding='same'))
-model.add(layers.Dropout(0.4))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dropout(0.3))
-model.add(layers.Dense(NUM_CLASSES, activation='softmax'))
+        # build the sequential network
+        self.model = keras.Sequential()
+        self.model.add(layers.Conv2D(24, kernel_size=(self.KERNEL_SIZE, self.KERNEL_SIZE), activation='relu', padding='same', input_shape=(self.dataset.SAMPLE_SHAPE[0], self.dataset.SAMPLE_SHAPE[1], 1)))
+        self.model.add(layers.MaxPooling2D(pool_size=(4, 2), strides=(4, 2), padding ='same'))
+        self.model.add(layers.Dropout(0.25))
+        self.model.add(layers.Conv2D(48, (self.KERNEL_SIZE, self.KERNEL_SIZE), activation='relu', padding='same'))
+        self.model.add(layers.MaxPooling2D(pool_size=(4, 2), strides=(4, 2), padding ='same'))
+        self.model.add(layers.Dropout(0.25))
+        self.model.add(layers.Conv2D(48, (self.KERNEL_SIZE, self.KERNEL_SIZE), activation='relu', padding='same'))
+        self.model.add(layers.Dropout(0.4))
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(64, activation='relu'))
+        self.model.add(layers.Dropout(0.3))
+        self.model.add(layers.Dense(self.NUM_CLASSES, activation='softmax'))
 
-# display model summary
-model.summary()
+        # display model summary
+        self.model.summary()
 
-# optimize model using cross entropy and the 'Adam" optimizer
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+    def train_model(self) -> None:
+        """Optimizes the model, sets up TensorBoard output, and trains the model
 
-# setup tensorboard
-log_dir = LOG_DIR + datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        """
+        # optimize model using cross entropy and the 'Adam" optimizer
+        self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 
-# train model
-train = model.fit(train_x, 
-                  train_y_one_hot, 
-                  batch_size=BATCH_SIZE, 
-                  epochs=EPOCHS,
-                  verbose=1,
-                  validation_data=(test_x, test_y_one_hot),
-                  callbacks=[tensorboard_callback])
+        # setup tensorboard
+        log_dir = self.LOG_DIR + datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+        tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
+        # train model
+        train = self.model.fit(self.train_x, 
+                               self.train_y_one_hot, 
+                               batch_size=self.BATCH_SIZE, 
+                               epochs=self.EPOCHS,
+                               verbose=1,
+                               validation_data=(self.test_x, self.test_y_one_hot),
+                               callbacks=[tensorboard_callback])
+
+if __name__ == "__main__":
+    training_data_path = '/home/hiraj/projects/ear-uci-dataset/spectrograms/train'
+    validation_data_path = '/home/hiraj/projects/ear-uci-dataset/spectrograms/validation'
+    network = Network(training_data_path, validation_data_path)
+    network.prepare_dataset()
+    network.create_network()
+    network.train_model()
