7fbe4c99b468083bafc385bfe65e03fd598aba21
Jigar Hira <hiraj@uci.edu>
2021-01-12 13:08:36 -0800

    updated prediction

--- prediction.py
+++ prediction.py
@@ -15,18 +15,30 @@ model_path = './saved_models'
 model = load_model(model_path, compile = True)
 
 #Insert samples
-samples = []
-use_samples = [12, 354, 2, 94, 123, 1003, 843, 253, 41, 2365]
-prediction_path = 'C:/Users/Ian/EAR-UCI/dataset/'
+samples_x = []
+samples_y = []
+
+#use_samples = [12, 354, 2, 94, 123, 1003, 843, 253, 41, 2365]
+use_samples = [x for x in range(10)]
+
+prediction_path = './dataset'
 prediction_x = np.load(prediction_path+'/test_x.npy', allow_pickle=True)
 print(prediction_x.shape)
 prediction_y = np.load(prediction_path+'/test_y.npy', allow_pickle=True)
-for sample in use_samples:
-    samples.append(prediction_x[0,sample])
+print(prediction_y.shape)
 
-samples = np.array(samples)
-print(samples.shape)
+for sample in use_samples:
+    samples_x.append(prediction_x[0, sample])
+    samples_y.append(prediction_y[0, sample])
+
+samples_x = np.array(samples_x)
+samples_y = np.array(samples_y)
+samples_x = np.expand_dims(samples_x, axis=-1)
+samples_y = np.expand_dims(samples_y, axis=-1)
+print(samples_x.shape)
+print(samples_y.shape)
 
 #Generate predictions for samples
-predictions = model.predict(samples)
-print(predictions)
+predictions = model.predict(samples_x, batch_size=1)
+print(np.argmax(predictions, axis=1))
+print(samples_y.flatten().astype('int'))
