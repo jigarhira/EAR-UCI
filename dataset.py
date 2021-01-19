7048214c129fe7843011deb9a0bad5b99cfb415a
Jigar Hira <hiraj@uci.edu>
2021-01-19 11:11:22 -0800

    added configuration to model

--- dataset.py
+++ dataset.py
@@ -8,9 +8,13 @@ Author: Jigar Hira
 
 import os
 import numpy as np
+from typing import Tuple
 
 
 class EARDataset:
+    """EAR project dataset generation, loading, and attributes.
+    
+    """
 
     # dataset parameters
     SAMPLE_CATEGORIES = [0, 1, 2, 3]
@@ -29,7 +33,38 @@ class EARDataset:
         self.test_y = np.zeros((self.VALIDATION_FOLDS, self.SAMPLES_PER_FOLD))
 
 
-    def load(self, training_data_path, validation_data_path, output_file_path):
+    def load(self, dataset_file_path:str) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """Loads dataset binaries from file and returns the sample sets.
+
+        Args:
+            dataset_file_path (str): folder path to binaries
+
+        Returns:
+            np.ndarray, np.ndarray, np.ndarray, np.ndarray: train_x, train_y, test_x, test_y
+        """
+        # load dataset binaries
+        print('Loading dataset files')
+        try:
+            train_x = np.load(dataset_file_path + 'train_x.npy', allow_pickle=True)
+            train_y = np.load(dataset_file_path + 'train_y.npy', allow_pickle=True)
+            test_x = np.load(dataset_file_path + 'test_x.npy', allow_pickle=True)
+            test_y = np.load(dataset_file_path + 'test_y.npy', allow_pickle=True)
+            print('Dataset loading complete')
+        except FileNotFoundError as e:
+            print('Binaries could not be found in ' + dataset_file_path + '\nRun generate before load\n', e)
+            return None, None, None, None
+
+        return train_x, train_y, test_x, test_y
+    
+    
+    def generate(self, training_data_path:str, validation_data_path:str, output_file_path:str):
+        """Generates dataset binaries from spectrogram files.
+
+        Args:
+            training_data_path (str): folder path to training data
+            validation_data_path (str): folder path to validation data
+            output_file_path (str): output file path
+        """
         count = 0
 
         print('Loading test set')
@@ -79,7 +114,7 @@ class EARDataset:
                     print('loaded ' + str(count) + ' spectrograms')
 
         # save dataset to numpy arrays
-        print('saving dataset to files')
+        print('Saving dataset to files')
         np.save(output_file_path + 'train_x.npy', self.train_x, allow_pickle=True)
         np.save(output_file_path + 'train_y.npy', self.train_y, allow_pickle=True)
         np.save(output_file_path + 'test_x.npy', self.test_x, allow_pickle=True)
@@ -87,4 +122,5 @@ class EARDataset:
 
 
 if __name__ == "__main__":
-    pass
+    dataset = EARDataset()
+    dataset.load('.')
