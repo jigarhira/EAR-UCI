76b1bb19015442291c08f0dc0a2b0e07a241e377
ihflores <ianhawkflores@gmail.com>
2021-01-31 13:14:12 -0800

    Added tutorial code for sms notifications

---
+++ sms_test.py
@@ -0,0 +1,16 @@
+import os
+from twilio.rest import Client
+
+account_sid = '***REMOVED***'
+auth_token = '***REMOVED***'
+client = Client(account_sid, auth_token)
+twilio_num = '***REMOVED***'
+user_num = '***REMOVED***'
+
+message = client.messages \
+                .create(
+                    messaging_service_sid='MG5731096200843a89dfe032f3e807ffe9',
+                    body="Testing twilio sms messaging.",
+                    to=user_num
+                )
+print(message.sid)
