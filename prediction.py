d1a8f0f05801886779ab7206ed85b1f30d07c513
ihflores <ianhawkflores@gmail.com>
2021-02-04 11:42:24 -0800

    fixed prediction, now normalizes inputs before predicting

--- prediction.py
+++ prediction.py
@@ -18,9 +18,9 @@ model = load_model(model_path, compile = True)
 samples_x = []
 samples_y = []
 
-use_samples = [12, 354, 2, 94, 123, 1003, 843, 253, 41, 2365]
+#use_samples = [12, 354, 2, 94, 123, 1003, 843, 253, 41, 456]
 total = 100
-#use_samples = [x for x in range(total)]
+use_samples = [x for x in range(total)]
 
 prediction_path = './dataset'
 prediction_x = np.load(prediction_path+'/test_x.npy', allow_pickle=True)
@@ -29,13 +29,18 @@ prediction_y = np.load(prediction_path+'/test_y.npy', allow_pickle=True)
 print(prediction_y.shape)
 
 for sample in use_samples:
-    samples_x.append(prediction_x[0, sample])
-    samples_y.append(prediction_y[0, sample])
+    samples_x.append(prediction_x[sample])
+    samples_y.append(prediction_y[sample])
 
 samples_x = np.array(samples_x)
 samples_y = np.array(samples_y)
+
+for i, sample in enumerate(samples_x):
+    samples_x[i] = (sample - sample.min()) / (sample.max() - sample.min())
+
 samples_x = np.expand_dims(samples_x, axis=-1)
 samples_y = np.expand_dims(samples_y, axis=-1)
+
 print(samples_x.shape)
 print(samples_y.shape)
 
