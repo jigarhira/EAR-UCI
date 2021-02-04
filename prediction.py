2754880578a54dc008e179a603e6cf7e57a97715
ihflores <ianhawkflores@gmail.com>
2021-02-04 11:54:09 -0800

    added random prediction

--- prediction.py
+++ prediction.py
@@ -8,6 +8,7 @@ Author: Ian Flores
 
 import tensorflow as tf
 import numpy as np
+from random import randint
 from tensorflow.keras.models import load_model
 
 #Load the saved model
@@ -18,14 +19,17 @@ model = load_model(model_path, compile = True)
 samples_x = []
 samples_y = []
 
-use_samples = [12, 354, 2, 94, 123, 1003, 843, 253, 41, 456]
-#use_samples = [x for x in range(100)]
-
 prediction_path = './dataset'
 prediction_x = np.load(prediction_path+'/test_x.npy', allow_pickle=True)
 print(prediction_x.shape)
 prediction_y = np.load(prediction_path+'/test_y.npy', allow_pickle=True)
 print(prediction_y.shape)
+
+#create random prediction variables
+use_samples = []
+predictions = 100
+for i in range(predictions):
+    use_samples.append(randint(0,len(prediction_x)-1))
 
 for sample in use_samples:
     samples_x.append(prediction_x[sample])
