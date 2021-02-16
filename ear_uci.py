c69bb96e682d1b5c0160fa268ecf970b51dfc5d5
ihflores <ianhawkflores@gmail.com>
2021-02-16 11:23:30 -0800

    cleaned phone num line

--- ear_uci.py
+++ ear_uci.py
@@ -13,7 +13,7 @@ account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 twilio_num = '***REMOVED***'
-user_num = '***REMOVED***'
+user_num = os.getenv("PHONE_NUM")
 
 
 def main():
