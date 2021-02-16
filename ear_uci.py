17bc71b66b4f956469034d2db9bf8e69aa17c227
ihflores <ianhawkflores@gmail.com>
2021-02-16 11:21:53 -0800

    added basic notification in loop

--- ear_uci.py
+++ ear_uci.py
@@ -1,10 +1,19 @@
 import time
+import os
 import numpy as np
 import librosa
+from tensorflow.python.keras.backend import switch
+from twilio.rest import Client
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 from audio import Audio
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+twilio_num = '***REMOVED***'
+user_num = '***REMOVED***'
 
 
 def main():
@@ -16,6 +25,8 @@ def main():
     audio = Audio()
     audio.start_live_record()
 
+    last_prediction = 10
+    count = 0
     # prediction loop
     while True:
         time_start = time.time()
@@ -35,6 +46,38 @@ def main():
         # generate prediction
         prediction = model.predict(spectrogram, batch_size=1)
         prediction = np.argmax(prediction, axis=1)
+
+        # increment counter if current prediction matches last prediction
+        if(prediction == last_prediction):
+            count = count + 1
+        else:
+            count = 0
+
+        #update last_prediction
+        last_prediction = prediction
+
+        if(count >= 3):
+            if(prediction == 1):
+                message = client.messages \
+                    .create(
+                        messaging_service_sid='MG5731096200843a89dfe032f3e807ffe9',
+                        body="EAR has detected glass breaking.",
+                        to=user_num
+                    )
+            elif(prediction == 2):
+                message = client.messages \
+                    .create(
+                        messaging_service_sid='MG5731096200843a89dfe032f3e807ffe9',
+                        body="EAR has detected a gunshot.",
+                        to=user_num
+                    )
+            elif(prediction == 3):
+                message = client.messages \
+                    .create(
+                        messaging_service_sid='MG5731096200843a89dfe032f3e807ffe9',
+                        body="EAR has detected a scream.",
+                        to=user_num
+                    )                    
 
         print(prediction)
         print('loop time: ' + str(time.time() - time_start))
